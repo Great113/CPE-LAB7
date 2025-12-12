75425abef6a568299ac4b4edd517f4c90606f2da
Korrawit <greater2316@gmail.com>
2025-12-12 17:05:01 +0700

    yes

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,3 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+string x;
+int y;
+string r;
+string p;
+string t;
+cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
+cout << "?????: ";
+getline(cin,x);
+cout << "Fahsai: Wow!!! " << x << " is a really cool name.\n";
+cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
+cout << x << ": " ;
+cin >> y ;
+cin.ignore();
+cout << "Fahsai: I think you may be GEAR "<< (y/10000000)-12 <<" I have a free movie ticket for you.\n";
+cout << "Fahsai: Let's go to the cinema together!!!\n";
+cout << "Fahsai: What movie do you want to watch?\n";
+cout << x <<":";
+getline(cin,r);
+cout << "\nFahsai: So....which day are you free to go with me?\n";
+cout << x << ":  " ;
+getline(cin,p);
+cin.ignore();
+cout <<"Fahsai: " << p <<"....that is OK!!! I'm looking forward to watching " << r << " with you.\n";
+cout << x << ": " ;
+getline(cin,t);
+cout << "Fahsai: 555+ see you " << p <<". Bye Bye \\(^ ^)/";
+
+}
 //Dialog of the first example is given below.
 
 /*
